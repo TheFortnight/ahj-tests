aa93524d1ed2e4883c83ca79d8908cacad9178ec
Александр Алябьев <aleksandr.b.aliabev@phrm.tech>
2023-09-09 15:05:34 +0300

    tests to test

--- test/defBank_2.test.js
+++ test/defBank_2.test.js
@@ -2,8 +2,8 @@ const defBank = require('../src/defBank.js');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
   await page.goto('http://localhost:8081/')
   await page.waitForSelector(`.input`, {timeout: 10000, visible: true});
   await page.type('.input', '4444');
